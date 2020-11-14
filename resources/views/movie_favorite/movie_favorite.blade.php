62e9d7b7c44d64b31ad5d28c9e316a3564b53fce
hirohito.hayama <hiro.hellotheworld@gmail.com>
2020-11-14 08:56:25 +0000

    welcome_page_revise

--- resources/views/movie_favorite/movie_favorite.blade.php
+++ resources/views/movie_favorite/movie_favorite.blade.php
@@ -7,10 +7,10 @@
                     <div>
                         {{-- タイトル --}}
                         <p class="ml-10" style= 'margin-left: 50px'><?php print htmlspecialchars($movie->title , ENT_QUOTES, "UTF-8"); ?></p>
-                    <div class='delete'>
+                    <div class='user_buttons'>
                         {{-- unfavoriteボタンのフォーム --}}
                         {!! Form::open(['route' => ['users.detach_favorite', $movie_id[$key]->id], 'method' => 'delete']) !!}
-                            {!! Form::submit('お気に入り削除', ['class' => "btn-success btn-sm"]) !!}
+                            {!! Form::submit('お気に入り削除', ['class' => "btn-danger btn-lg",'style' => 'width:200px']) !!}
                         {!! Form::close() !!}
                     </div>
                     </div>
