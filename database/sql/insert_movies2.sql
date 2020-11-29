a5b6763d4ce46362e1fe0f2938c1e3adfc506c92
hirohito.hayama <hiro.hellotheworld@gmail.com>
2020-11-29 06:19:49 +0000

    add_seeder3

--- database/sql/insert_movies2.sql
+++ database/sql/insert_movies2.sql
@@ -1,80 +1,80 @@
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(151,"ジュブナイル",68526,2,1,1);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(152,"ぼくは明日、昨日のきみとデートする",428099,2,1,1);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(153,"リターナー",5493,2,1,1);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(154,"牙狼〈GARO〉スペシャル 白夜の魔獣",66744,2,1,1);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(155,"時をかける少女",62385,2,1,1);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(156,"増山超能力師事務所　～激情版は恋の味～",523274,2,2,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(151,'ジュブナイル',68526,2,1,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(152,'ぼくは明日、昨日のきみとデートする',428099,2,1,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(153,'リターナー',5493,2,1,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(154,'牙狼〈GARO〉スペシャル 白夜の魔獣',66744,2,1,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(155,'時をかける少女',62385,2,1,1);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(156,'増山超能力師事務所　～激情版は恋の味～',523274,2,2,1);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(157,"ヒメアノ～ル",394448,2,1,2);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(158,"怒り",359344,2,1,2);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(159,"八日目の蝉",94054,2,1,2);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(160,"検察側の罪人",517373,2,1,2);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(157,'ヒメアノ～ル',394448,2,1,2);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(158,'怒り',359344,2,1,2);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(159,'八日目の蝉',94054,2,1,2);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(160,'検察側の罪人',517373,2,1,2);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(161,"小さな恋のうた",596727,2,1,3);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(162,"ちょっと今から仕事やめてくる",474626,2,1,3);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(163,"七つの会議",552686,2,1,3);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(164,"万引き家族",505192,2,1,3);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(165,"嫌われ松子の一生",31512,2,1,3);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(161,'小さな恋のうた',596727,2,1,3);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(162,'ちょっと今から仕事やめてくる',474626,2,1,3);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(163,'七つの会議',552686,2,1,3);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(164,'万引き家族',505192,2,1,3);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(165,'嫌われ松子の一生',31512,2,1,3);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(166,"いま、会いにゆきます",36092,2,1,4);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(167,"あやしい彼女",394310,2,1,4);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(168,"DESTINY 鎌倉ものがたり",492136,2,1,4);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(169,"トワイライト ささらさや",317221,2,1,4);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(170,"母と暮せば",380341,2,1,4);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(166,'いま、会いにゆきます',36092,2,1,4);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(167,'あやしい彼女',394310,2,1,4);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(168,'DESTINY 鎌倉ものがたり',492136,2,1,4);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(169,'トワイライト ささらさや',317221,2,1,4);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(170,'母と暮せば',380341,2,1,4);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(171,"君の膵臓をたべたい",449132,2,1,5);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(172,"恋は雨上がりのように ",520576,2,1,5);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(173,"小さな恋のうた",596727,2,1,5);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(174,"ビリギャル",349498,2,1,5);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(175,"スワロウテイル",8426,2,1,5);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(171,'君の膵臓をたべたい',449132,2,1,5);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(172,'恋は雨上がりのように ',520576,2,1,5);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(173,'小さな恋のうた',596727,2,1,5);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(174,'ビリギャル',349498,2,1,5);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(175,'スワロウテイル',8426,2,1,5);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(176,"ALWAYS　三丁目の夕日",13430,2,1,6);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(177,"ALWAYS 三丁目の夕日 '64",121239,2,1,6);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(178,"ALWAYS 続・三丁目の夕日",41177,2,1,6);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(179,"旅猫リポート",512340,2,1,6);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(180,"ひまわりと子犬の7日間",223391,2,1,6);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(176,'ALWAYS　三丁目の夕日',13430,2,1,6);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(177,'ALWAYS 三丁目の夕日 '64',121239,2,1,6);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(178,'ALWAYS 続・三丁目の夕日',41177,2,1,6);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(179,'旅猫リポート',512340,2,1,6);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(180,'ひまわりと子犬の7日間',223391,2,1,6);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(181,"ザ・ファブル",532267,2,1,7);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(182,"るろうに剣心 京都大火編",221731,2,1,7);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(183,"るろうに剣心 伝説の最期編",221732,2,1,7);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(184,"キングダム ",554805,2,1,7);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(185,"GOEMON",24293,2,1,7);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(181,'ザ・ファブル',532267,2,1,7);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(182,'るろうに剣心 京都大火編',221731,2,1,7);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(183,'るろうに剣心 伝説の最期編',221732,2,1,7);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(184,'キングダム ',554805,2,1,7);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(185,'GOEMON',24293,2,1,7);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(186,"呪怨 劇場版",11838,2,1,9);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(187,"呪怨2 劇場版",9666,2,1,9);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(188,"リング",2671,2,1,9);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(189,"仄暗い水の底から",12205,2,1,9);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(190,"来る",505916,2,1,9);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(186,'呪怨 劇場版',11838,2,1,9);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(187,'呪怨2 劇場版',9666,2,1,9);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(188,'リング',2671,2,1,9);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(189,'仄暗い水の底から',12205,2,1,9);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(190,'来る',505916,2,1,9);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(191,"岳 -ガク-",93178,2,1,10);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(192,"ジュブナイル",68526,2,1,10);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(193,"栄光への5000キロ",318728,2,1,10);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(194,"探検隊の栄光",390031,2,1,10);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(191,'岳 -ガク-',93178,2,1,10);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(192,'ジュブナイル',68526,2,1,10);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(193,'栄光への5000キロ',318728,2,1,10);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(194,'探検隊の栄光',390031,2,1,10);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(195,"永遠の0",248087,2,1,11);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(196,"アルキメデスの大戦",605389,2,1,11);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(197,"戦場のメリークリスマス",11948,2,1,11);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(198,"男たちの大和／YAMATO",19659,2,1,11);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(199,"少年時代",83419,2,1,11);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(195,'永遠の0',248087,2,1,11);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(196,'アルキメデスの大戦',605389,2,1,11);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(197,'戦場のメリークリスマス',11948,2,1,11);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(198,'男たちの大和／YAMATO',19659,2,1,11);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(199,'少年時代',83419,2,1,11);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(200,"彼女がその名を知らない鳥たち",456261,2,1,12);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(201,"百円の恋",314606,2,1,12);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(202,"ラストレター",542807,2,1,12);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(203,"空気人形",25053,2,1,12);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(204,"博士の愛した数式",89116,2,1,12);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(200,'彼女がその名を知らない鳥たち',456261,2,1,12);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(201,'百円の恋',314606,2,1,12);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(202,'ラストレター',542807,2,1,12);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(203,'空気人形',25053,2,1,12);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(204,'博士の愛した数式',89116,2,1,12);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(205,"カメラを止めるな！",513434,2,1,13);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(206,"セトウツミ",382200,2,1,13);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(207,"探偵はBARにいる",91989,2,1,13);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(208,"探偵はBARにいる2 ススキノ大交差点",238127,2,1,13);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(209,"超高速！参勤交代",279049,2,1,13);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(210,"超高速！参勤交代 リターンズ",442321,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(205,'カメラを止めるな！',513434,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(206,'セトウツミ',382200,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(207,'探偵はBARにいる',91989,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(208,'探偵はBARにいる2 ススキノ大交差点',238127,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(209,'超高速！参勤交代',279049,2,1,13);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(210,'超高速！参勤交代 リターンズ',442321,2,1,13);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(211,"不確かなメロディー",538582,2,1,14);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(212,"生きとし生けるもの",571460,2,1,14);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(213,"夢は牛のお医者さん",342774,2,1,14);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(214,"人生フルーツ",512350,2,1,14);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(215,"Start Line",456313,2,1,14);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(211,'不確かなメロディー',538582,2,1,14);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(212,'生きとし生けるもの',571460,2,1,14);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(213,'夢は牛のお医者さん',342774,2,1,14);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(214,'人生フルーツ',512350,2,1,14);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(215,'Start Line',456313,2,1,14);
 
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(216,"星くず兄弟の新たな伝説",603516,2,1,15);
-INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(217,"ダンスウィズミー",596445,2,1,15);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(216,'星くず兄弟の新たな伝説',603516,2,1,15);
+INSERT INTO movies (id,title,tmdb_id,west_east,live_animated,genre) VALUES(217,'ダンスウィズミー',596445,2,1,15);
